4482f2eb464fb529686e46872eb12956d4c3a6d9
Kapil <kapil2978@gmail.coom>
2021-09-06 19:12:08 -0400

    Bypass the config file, use env

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
 
 var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 module.exports = connection;
